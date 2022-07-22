11b402ce2bd50f64b03f0e3aa06990652cbbb7ed
cxy1 <cxy1@163.com>
2022-07-22 19:35:31 +0800

    "首次提交工程"

---
+++ python_pro/day03/__init__.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+__init__.py.py
+Created on 2022/7/22 19:31
+Copyright (c) 2022/7/22, 海牛学院版权所有.
+@author: pikka
+"""
---
+++ python_pro/day03/git_demo.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+"""
+git_demo.py
+Created on 2022/7/22 19:31
+Copyright (c) 2022/7/22, 海牛学院版权所有.
+@author: pikka
+"""
+a = 1
